da53fa55913673eddae2378c4b98d8d53e8b8171
Valeriia tymchenko <lera.tymchenko@ukr.net>
2024-03-27 20:45:21 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/V21Tymchenko//textarea-test",
+  base: "/V21Tymchenko/textarea-test",
 });
