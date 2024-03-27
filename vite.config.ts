e7d0e8ff404e5e2e9e8825da080f2809ff71720f
Valeriia tymchenko <lera.tymchenko@ukr.net>
2024-03-27 21:22:51 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/textarea-test/" : "/",
+  base: "/textarea-test/",
   build: {
-    outDir: "docs",
+    outDir: "dist",
     assetsDir: ".",
   },
 });
